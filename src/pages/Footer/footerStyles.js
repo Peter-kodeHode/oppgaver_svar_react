ed88615404316084310c69e3e6440e076c05614a
Peter-kodeHode <peter.kodehode@gmail.com>
2022-03-17 12:27:54 +0100

    Set footer to fixed position

--- src/pages/Footer/footerStyles.js
+++ src/pages/Footer/footerStyles.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 export const FooterStyled = styled.div`
+    position:fixed;
+    bottom:0;
     background-color: black;
     width:100%;
     height:100px;
 `
 
 export const FooterH1 = styled.h1`
+    margin:25px auto;
     color:cyan;
-    padding-top:25px;
 `
